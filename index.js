b5fd78a1358220c120b59cffb10910def05c7c74
Nabanita Bag <ashley.makruz@gmail.com>
2021-01-31 00:53:14 +0530

    added background images and sound to the orchestra

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("hols");
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrums; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    var audio = new Audio("src/tom-" + 2 + ".mp3");
+    audio.play();
+  });
+}
