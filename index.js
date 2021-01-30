c496a5b816156a7f5d3e440a9e8851db55c88315
Nabanita Bag <ashley.makruz@gmail.com>
2021-01-31 01:49:39 +0530

    added sounds via keys and on clicking

--- index.js
+++ index.js
@@ -2,7 +2,45 @@ var numberOfDrums = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrums; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function () {
-    var audio = new Audio("src/tom-" + 2 + ".mp3");
-    audio.play();
+    playKeywise(this.innerHTML);
   });
+}
+
+document.addEventListener("keydown", function (ev) {
+  playKeywise(ev.key);
+});
+
+function playKeywise(x) {
+  switch (x) {
+    case "w":
+      var tom1 = new Audio("src/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("src/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("src/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("src/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var crash = new Audio("src/crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      var kick = new Audio("src/kick-bass.mp3");
+      kick.play();
+      break;
+    case "l":
+      var snare = new Audio("src/snare.mp3");
+      snare.play();
+      break;
+    default:
+      console.log(x);
+  }
 }
