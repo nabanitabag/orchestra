11784df23f1fa699ceb6e9ab94cec3c381783043
Nabanita Bag <ashley.makruz@gmail.com>
2021-02-17 19:12:06 +0530

    variable name change

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ function playKeywise(x) {
       tom1.play();
       break;
     case "a":
-      var tom2 = new Audio("src/tom-2.mp3");
-      tom2.play();
+      var Tom2 = new Audio("src/tom-2.mp3");
+      Tom2.play();
       break;
     case "s":
-      var tom3 = new Audio("src/tom-3.mp3");
-      tom3.play();
+      var Tom3 = new Audio("src/tom-3.mp3");
+      Tom3.play();
       break;
     case "d":
       var tom4 = new Audio("src/tom-4.mp3");
